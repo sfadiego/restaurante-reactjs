e931fdbc309d46ab692401ba9bbe983a8f517d36
sfadiego <sfadiego@gmail.com>
2021-07-20 15:36:53 -0500

    primer commit template

---
+++ src/layout/RestaurantComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from 'antd';
+
+export const RestaurantComponent = () => {
+    return (
+        <>
+            <Button type="primary">Button</Button>
+        </>
+    )
+}
